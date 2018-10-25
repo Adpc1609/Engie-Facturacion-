a3543c440a47fe46746ef6b53dcbabc483b69179
Adriana Perez <adpc1609@gmail.com>
2018-10-25 05:33:24 -0300

    cambios en el grahInfo

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,4 @@
-//funcionalidad del Menú lateral
+/* //funcionalidad del Menú lateral
 function toggleMenu() {
 	if (sideMenu.className.indexOf("menu_closed") >= 0) { 
 	  openMenu(); 
@@ -15,5 +15,19 @@ function toggleMenu() {
   function closeMenu() {
 	sideMenu.classList.add('menu_closed'); 
 	sideMenu.classList.remove('menu_open');
-  }
-  
+	} */
+
+ /*navbar*/
+ //funcionalidad del Menú lateral/* Set the width of the side navigation to 250px */
+ //funcionalidad del Menú lateral/* Set the width of the side navigation to 250px */
+function openNav() {
+	document.getElementById("mySidenav").style.width = "250px";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+	document.getElementById("mySidenav").style.width = "0";
+}
+$('#datepicker').datepicker({
+	uiLibrary: 'bootstrap4'
+});
